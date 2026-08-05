58a6ee1576ec90f7341be6f680fc7cfeee647ec5
kaixin1995 <admin@haokaikai.cn>
2026-08-06 05:47:36 +0800

    补充聊天流式消息自动滚动

--- src/AITool.Desktop/Views/ChatView.axaml.cs
+++ src/AITool.Desktop/Views/ChatView.axaml.cs
@@ -1,8 +1,90 @@
+using System.Collections.Specialized;
+using System.ComponentModel;
 using Avalonia.Controls;
+using Avalonia.Threading;
+using AITool.Desktop.Models;
+using AITool.Desktop.ViewModels;
 
 namespace AITool.Desktop.Views;
 
 public partial class ChatView : UserControl
 {
-    public ChatView() => InitializeComponent();
+    private ChatViewModel? _viewModel;
+
+    public ChatView()
+    {
+        InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        if (_viewModel is not null)
+        {
+            _viewModel.Messages.CollectionChanged -= OnMessagesChanged;
+            foreach (var message in _viewModel.Messages)
+            {
+                message.PropertyChanged -= OnMessagePropertyChanged;
+            }
+        }
+
+        _viewModel = DataContext as ChatViewModel;
+        if (_viewModel is null) return;
+
+        _viewModel.Messages.CollectionChanged += OnMessagesChanged;
+        foreach (var message in _viewModel.Messages)
+        {
+            message.PropertyChanged += OnMessagePropertyChanged;
+        }
+
+        QueueScrollToEnd(true);
+    }
+
+    private void OnMessagesChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.OldItems is not null)
+        {
+            foreach (ChatMessage message in e.OldItems)
+            {
+                message.PropertyChanged -= OnMessagePropertyChanged;
+            }
+        }
+
+        if (e.NewItems is not null)
+        {
+            foreach (ChatMessage message in e.NewItems)
+            {
+                message.PropertyChanged += OnMessagePropertyChanged;
+            }
+        }
+
+        QueueScrollToEnd(true);
+    }
+
+    private void OnMessagePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(ChatMessage.Content)
+            or nameof(ChatMessage.Reasoning)
+            or nameof(ChatMessage.Attempts)
+            or nameof(ChatMessage.TotalDurationMs)
+            or nameof(ChatMessage.IsStreaming)
+            or nameof(ChatMessage.IsError))
+        {
+            QueueScrollToEnd(false);
+        }
+    }
+
+    private void QueueScrollToEnd(bool force)
+    {
+        Dispatcher.UIThread.Post(() =>
+        {
+            var isNearBottom = MessagesScrollViewer.Offset.Y
+                + MessagesScrollViewer.Viewport.Height
+                >= MessagesScrollViewer.Extent.Height - 32;
+            if (force || isNearBottom)
+            {
+                MessagesScrollViewer.ScrollToEnd();
+            }
+        }, DispatcherPriority.Background);
+    }
 }
